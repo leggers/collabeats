1aa082cb882c93f1b6e7d2db332ee2d261acadf6
Lucas Eggers <l.eggers91@gmail.com>
2014-03-01 18:43:35 -0300

    got a sound working on button press

--- collabeats.js
+++ collabeats.js
@@ -8,6 +8,12 @@ if (Meteor.isClient) {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
+        var sound = new Howl({
+          urls: ['/808_Original.wav'],
+          autoplay: true,
+          onloaderror: function() {console.log('error!');},
+          onload: function() {console.log('error!');}
+        });
     }
   });
 }
