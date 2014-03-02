938bf645a12d9232c63c2a2f3461d90db33dd19a
Lucas Eggers <l.eggers91@gmail.com>
2014-03-02 15:33:32 -0300

    got a loop of sound

--- collabeats.js
+++ collabeats.js
@@ -3,17 +3,45 @@ if (Meteor.isClient) {
     return "Welcome to collabeats.";
   };
 
+  Template.hello.getInterval = function() {
+    return getInterval();
+  };
+
   var sound = new Howl({
     urls: ['/808-sample-pack/Closed-Hihat/closedHat1.mp3'],
     onloaderror: function() {console.log('error!');},
     onload: function() {console.log('loaded');}
   });
 
+  var looping = false;
+  var tempo = 124;
+
+  var loopFunc = function() {
+    console.log('looping');
+    if (looping) {
+      sound.play();
+      setTimeout(function(){
+        loopFunc();
+      }, getInterval());
+    }
+  };
+
+  var getInterval = function() {
+    return tempo;
+  };
+
   Template.hello.events({
-    'click input': function () {
+    'click input#play-once': function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         sound.play();
+    },
+    'click input#start-beat': function() {
+      looping = true;
+      loopFunc();
+    },
+    'change input#tempo': function() {
+      console.log(this);
     }
   });
 }
