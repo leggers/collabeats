de3743e886f3ce0b89d3ffd1b3655f230f820611
Lucas Eggers <l.eggers91@gmail.com>
2014-03-02 15:17:43 -0300

    aifs do not work, converted some files to mp3 to play with

--- collabeats.js
+++ collabeats.js
@@ -3,17 +3,17 @@ if (Meteor.isClient) {
     return "Welcome to collabeats.";
   };
 
+  var sound = new Howl({
+    urls: ['/808-sample-pack/Closed-Hihat/closedHat1.mp3'],
+    onloaderror: function() {console.log('error!');},
+    onload: function() {console.log('loaded');}
+  });
+
   Template.hello.events({
     'click input': function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-        var sound = new Howl({
-          urls: ['/808_Original.wav'],
-          autoplay: true,
-          onloaderror: function() {console.log('error!');},
-          onload: function() {console.log('error!');}
-        });
+        sound.play();
     }
   });
 }
