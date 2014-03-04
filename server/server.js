5ef5dad7c279da45e19ad106a627389185551202
Lucas Eggers <l.eggers91@gmail.com>
2014-03-04 17:05:09 -0300

    current seed data looks good

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ Meteor.startup(function () {
   Channels.find().forEach(function (channel) {
     Meteor.call('removeChannel', {channelId: channel._id});
   });
+  Steps.find().forEach(function (step) {
+    Steps.remove({_id: step._id});
+  });
   var folder = '/808-sample-pack/';
   var urls = ['bass-drum/kickdrum0001.mp3',
               'closed-hihat/closed-hihat0001.mp3',
