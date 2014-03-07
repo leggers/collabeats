012cba9d378f6ce72463107bf4375425012ba81b
Lucas Eggers <l.eggers91@gmail.com>
2014-03-06 21:27:24 -0300

    added all sounds

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
 Meteor.startup(function () {
   var urls = ['bass-drum/kickdrum0001.mp3',
-              'closed-hihat/closed-hihat0001.mp3',
+              'snare-drum/snaredrum0001.mp3',
+              'misc/clap.mp3',
+              'misc/rimshot.mp3',
+              'misc/maraca.mp3',
+              'misc/clav.mp3',
               'cymbal/cymbal0001.mp3',
-              'misc/clap.mp3'
+              'closed-hihat/closed-hihat0001.mp3',
+              'open-hihat/open-hihat0001.mp3',
+              'low-conga/low-conga0001.mp3',
+              'low-tom/low-tom0001.mp3',
+              'mid-conga/mid-conga0001.mp3',
+              'mid-tom/mid-tom0001.mp3',
+              'hi-conga/hi-conga0001.mp3',
+              'hi-tom/hi-tom0001.mp3'
               ];
   if (Channels.find().count() != urls.length) {
     var folder = '/808-sample-pack/';
     for(var i = 0; i < urls.length; i++) {
       if (Channels.findOne({soundUrl: folder + urls[i]}) === undefined) {
+        var name = urls[i].substr(0, urls[i].indexOf('/'));
+        if (name === 'misc') name = urls[i].substr(urls[i].indexOf('/'), urls[i].indexOf('.'));
         Meteor.call('addChannel', {
           numSteps: 16,
           channelSoundUrl: folder + urls[i],
           roomId: 0,
-          channelName: urls[i].substr(0, urls[i].indexOf('/'))
+          channelName: name
         });
       }
     }
