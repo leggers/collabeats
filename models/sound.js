c4d8ca21f1a6db4282aa96eef5fa068b33d5e926
Lucas Eggers <l.eggers91@gmail.com>
2014-03-14 22:19:53 -0300

    started fleshing out sound model

--- models/sound.js
+++ models/sound.js
@@ -3,10 +3,10 @@
 /*
   Each sample is a doc in the Steps collection:
     name: sound name
-    variants: an Object that stores variations of a sound (say, different pitch, or sustain or whatever),
-      in the form { variantName: soundUrl, ... }
-    owner: ID of person who created the sound
-    private: only usable in a room where the owner is the room owner
+    variants: an Array that stores variations of a sound (say, different pitch, or sustain or whatever),
+      in the form [{ variantName: soundUrl }, ...]
+    ownerId: ID of person who created the sound
+    privateSound: only usable in a room where the owner is the room owner
       (else pull the sound from the room)
 */
 
@@ -24,5 +24,22 @@ Sounds.allow({
   remove: function () {
     // no cowboy removes; use removeSound method
     return false;
+  }
+});
+
+Meteor.methods({
+  addSound: function (name, variantName, variantUrl, privateSound) {
+    Sounds.insert({
+      ownerId: this.userId,
+      name: name,
+      variants: [ {variantName: variantUrl} ],
+      privateSound: privateSound
+    });
+  },
+  addVariant: function (soundId, variantName, variantUrl) {
+    sound = Sounds.findOne(soundId);
+    if (sound.ownerId == this.userId) {
+      Sounds.update(soundId, {$addToSet: {variantName: variantUrl}});
+    }
   }
 });
