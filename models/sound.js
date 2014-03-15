dcfe393317d5a250c6ad4930cc6c25590d8576f9
Lucas Eggers <l.eggers91@gmail.com>
2014-03-14 21:37:32 -0300

    styling

--- models/sound.js
+++ models/sound.js
@@ -5,6 +5,9 @@
     name: sound name
     variants: an Object that stores variations of a sound (say, different pitch, or sustain or whatever),
       in the form { variantName: soundUrl, ... }
+    owner: ID of person who created the sound
+    private: only usable in a room where the owner is the room owner
+      (else pull the sound from the room)
 */
 
 // Sounds = new Meteor.Collection('sto')
